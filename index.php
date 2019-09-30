eec44cfb4102f244ee58b23261212d92e0037e6a
Will L <horizon.william14@gmail.com>
2019-09-30 07:01:19 -0400

    Added monthly newsletter

--- index.php
+++ index.php
@@ -437,7 +437,34 @@
 						  <p>Toronto, Canada
 						  </p>
 					  </div>
-			    </div>
+				</div>
+				<div class="col-md 12">
+					<!-- Begin Mailchimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:#F2F2F2; clear:left; font:14px Helvetica,Arial,sans-serif; }
+	/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="https://gmail.us20.list-manage.com/subscribe/post?u=19b5484360b07ef43fd97b267&amp;id=be88e6b4d8" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll" style="text-align: center">
+	<h2>Subscribe to my Monthly Newsletter</h2>
+<div class="mc-field-group">
+	<input type="email" value="" name="EMAIL" placeholder="Enter email address ..." class="required email" id="mce-EMAIL" style="width: 60%; margin: auto; display: block; border-radius: 10px">
+</div>
+	<div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_19b5484360b07ef43fd97b267_be88e6b4d8" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" style="background-color: #218838;" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
+				</div>
 		    </div>
 	  </section>
 
