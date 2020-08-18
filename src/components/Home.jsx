fd4e01f286f59492f0a2176e4165675634ed0b7a
wlawt <williamlaw.wtl@gmail.com>
2020-08-17 22:18:26 -0400

    refactor newsletter

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import Now from "./homeSections/Now"
 import Software from "./homeSections/Software"
 import Consulting from "./homeSections/Consulting"
 import Article from "./homeSections/Article"
 import Design from "./homeSections/Design"
+import Newsletter from "./homeSections/Newsletter"
 
 export default function Home() {
-  const [emailValue, setEmail] = useState("")
-
   return (
     <>
       <Now />
@@ -29,55 +28,7 @@ export default function Home() {
           {" "}here.
         </a>
       </p>
-
-      <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css" />
-      <div id="mc_embed_signup" style={{ backgroundColor: "#252525" }}>
-        <form
-          action="https://gmail.us20.list-manage.com/subscribe/post?u=19b5484360b07ef43fd97b267&amp;id=be88e6b4d8"
-          method="post"
-          id="mc-embedded-subscribe-form"
-          name="mc-embedded-subscribe-form"
-          className="validate"
-          target="_blank"
-          noValidate
-        >
-
-          <div id="mc_embed_signup_scroll">
-            <input
-              type="email"
-              value={emailValue}
-              onChange={(e) => setEmail(e.target.value)}
-              name="EMAIL"
-              className="email"
-              id="mce-EMAIL"
-              placeholder="Email"
-              required
-            />
-
-            <div style={{ position: "absolute", left: "-5000px" }} aria-hidden="true">
-              <input
-                type="text"
-                name="b_19b5484360b07ef43fd97b267_be88e6b4d8"
-                tabIndex="-1" value=""
-                readOnly
-              />
-            </div>
-
-            <div className="clear">
-              <input
-                type="submit"
-                value="Subscribe"
-                name="subscribe"
-                id="mc-embedded-subscribe"
-                className="button"
-                style={{ backgroundColor: "black" }}
-                readOnly
-              />
-            </div>
-          </div>
-
-        </form>
-      </div>
+      <Newsletter />
 
       <br />
       <br />
