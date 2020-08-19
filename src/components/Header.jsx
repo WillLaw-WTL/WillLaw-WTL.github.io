1420c58ef4df0073baf1a134f2d395c07380992e
William Law <williamlaw.wtl@gmail.com>
2020-08-18 21:02:38 -0400

    restructure css framework (#26)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -40,37 +40,39 @@ export default function Header2() {
       <header>
         <h1>William Law</h1>
         <img src={Headshot} style={{ borderRadius: "10px" }} alt="Avatar" />
+
         <p>Typing for a living <span role="img" aria-labelledby="">🙃😎</span></p>
         <p>Computer Science @ uWaterloo '25</p>
+
         <p style={{ paddingBottom: 0, marginBottom: 0 }}>
           <b>Let's chat if you:</b> have an interesting startup opportunity in software/product
-              that can help lots of people <b>OR</b> a cool project that you're working on that you think can go somewhere
-          </p>
-        <div style={{ marginBottom: "10px", paddingBottom: "10px" }}>
-          {/* TODO: Add function to toggle theme */}
-          {/* <input type="checkbox" id="switch"></input><label htmlFor="switch">Toggle</label> */}
-        </div>
-        <div>
+          that can help lots of people <b>OR</b> a cool project that you're working on that you think can go somewhere
+        </p>
+
+        <div style={{ marginBottom: "10px", paddingBottom: "10px" }}></div>
+
+        {/* <div>
           <iframe
             src="https://github.com/sponsors/wlawt/button"
             title="Sponsor wlawt" height="35" width="107"
             style={{ border: 0, marginBottom: "10px" }}>
           </iframe>
-        </div>
+        </div> */}
 
         {!showBlog ? (
           <div className="view"><a href="#" onClick={() => setShowBlog(true)}>Blog</a></div>
         ) : (
             <div className="view"><a href="#" onClick={() => setShowBlog(false)}>Home</a></div>
           )}
+
         <br />
 
         {socials.map(social => (
           <div className="view" key={`${social.name}`}><a href={`${social.link}`}>{social.name}</a></div>
         ))}
         <div className="view" style={{ marginBottom: "10px" }}><a href={Resume} target="_blank" rel="noopener noreferrer">Resume</a></div>
-
       </header>
+
       <section>
         {!showBlog ? (
           <Home />
