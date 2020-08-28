91618ed6d2420f069af032d76b0a1ccec7127abb
wlawt <williamlaw.wtl@gmail.com>
2020-08-28 11:18:10 -0400

    update section + added internship

--- src/components/homeSections/Now.jsx
+++ src/components/homeSections/Now.jsx
@@ -4,16 +4,26 @@ export default function Now() {
 
   return (
     <>
+      <p>
+        Currently looking for Summer 2021 internships in infrastructure / backend / ML infrastructure
+        at any large company or medium-large startup!
+      </p>
       <h1>Now/Recent</h1>
       <ul>
         <li>
-          Contributing to
+          Reading on infrastructure / devops
+        </li>
+        <li>
+          Learning and practicing on system design
+        </li>
+        <li>
+          Contributed to
           <a href="https://github.com/SheetJS/js-word" target="_blank" rel="noopener noreferrer">
             {" "}WordJS - Word Processing Library
           </a>
         </li>
         <li>
-          Contributing to
+          Contributed to
           <a href="https://github.com/babel/sandboxes" target="_blank" rel="noopener noreferrer">
             {" "}Babel - Repl Sandboxes
           </a>
